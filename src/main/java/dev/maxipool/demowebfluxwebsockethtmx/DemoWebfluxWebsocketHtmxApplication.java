8fa3ac4de9c094542c272cb5cf5e70da51007ca1
maxiPool <maxime.berard@gmail.com>
2023-12-01 22:41:09 -0500

    chore: initial commit

---
+++ src/main/java/dev/maxipool/demowebfluxwebsockethtmx/DemoWebfluxWebsocketHtmxApplication.java
@@ -0,0 +1,13 @@
+package dev.maxipool.demowebfluxwebsockethtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebfluxWebsocketHtmxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebfluxWebsocketHtmxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/maxipool/demowebfluxwebsockethtmx/DemoWebfluxWebsocketHtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.maxipool.demowebfluxwebsockethtmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebfluxWebsocketHtmxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
